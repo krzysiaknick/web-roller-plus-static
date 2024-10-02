9c11d28fcd98752b4c37e77a43d7ebe7a7fbd2de
unknown <krzysiaknick@gmail.com>
2024-10-01 20:44:50 -0500

    inital

---
+++ script.js
@@ -0,0 +1,54 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function rollDiceForDiceElement(diceElement) {
+    const max = 6;
+    const min = 1;
+
+    let diceRoll = getRandomInt(min, max);
+
+    const dots = diceElement.querySelectorAll('.dot');
+    dots.forEach(dot => dot.style.display = 'none');
+
+    switch (diceRoll) {
+        case 1:
+            diceElement.querySelector('.one').style.display = 'block';
+            break;
+        case 2:
+            diceElement.querySelector('.two').style.display = 'block';
+            diceElement.querySelector('.five').style.display = 'block';
+            break;
+        case 3:
+            diceElement.querySelector('.one').style.display = 'block';
+            diceElement.querySelector('.two').style.display = 'block';
+            diceElement.querySelector('.five').style.display = 'block';
+            break;
+        case 4:
+            diceElement.querySelector('.two').style.display = 'block';
+            diceElement.querySelector('.three').style.display = 'block';
+            diceElement.querySelector('.four').style.display = 'block';
+            diceElement.querySelector('.five').style.display = 'block';
+            break;
+        case 5:
+            diceElement.querySelector('.one').style.display = 'block';
+            diceElement.querySelector('.two').style.display = 'block';
+            diceElement.querySelector('.three').style.display = 'block';
+            diceElement.querySelector('.four').style.display = 'block';
+            diceElement.querySelector('.five').style.display = 'block';
+            break;
+        case 6:
+            diceElement.querySelector('.two').style.display = 'block';
+            diceElement.querySelector('.three').style.display = 'block';
+            diceElement.querySelector('.four').style.display = 'block';
+            diceElement.querySelector('.five').style.display = 'block';
+            diceElement.querySelector('.six').style.display = 'block';
+            diceElement.querySelector('.seven').style.display = 'block';
+            break;
+    }
+}
+
+function rollAllDice() {
+    const diceElements = document.querySelectorAll('.dice');
+    diceElements.forEach(diceElement => rollDiceForDiceElement(diceElement));
+}
